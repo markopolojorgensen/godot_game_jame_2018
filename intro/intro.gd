8e7313eb04959dbd9f9767968418a5760a28397f
Mark J <marko.polo.jorgensen@gmail.com>
2018-06-15 11:36:20 -0500

    first commit, fancy intro

---
+++ intro/intro.gd
@@ -0,0 +1,26 @@
+extends Node
+
+func _ready():
+	$animation_player.connect("animation_finished", self, "animation_finished")
+	
+	resize_window()
+	
+	$animation_player.play("intro_fade_in")
+
+func resize_window():
+	OS.set_window_size(Vector2(320, 180) * 4)
+	
+	var screen_size = OS.get_screen_size(0)
+	var window_size = OS.get_window_size()
+	
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+
+
+
+func animation_finished(animation):
+	match animation:
+		"intro_fade_in":
+			$animation_player.play("intro_fade_out")
+		"intro_fade_out":
+			print("change to game scene")
+			get_tree().quit()
